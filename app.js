6c2eb9a0baee56596bea6a73f933f5109f44f746
milk-maid <roqbell@gmail.com>
2022-02-09 22:54:07 +0100

    on thr DOM

---
+++ app.js
@@ -0,0 +1,49 @@
+
+   /* let name = prompt(" what is your name?");
+     document.getElementById('trial').innerHTML = 'welcome: ' + name;
+     console.log('hello');
+     */
+// CONCATENATING
+
+let music = 'Get Rich',
+    artiste = 'Micheal Jackson';
+
+let  playing = 'Playing: ' + music + ' by ' + artiste;
+
+console.log(playing);
+
+//another concatenation
+
+let aim;
+ aim = 'Learning ';
+ aim += 'Javascript';
+//  aim += ' at home'; (this is also correct oo)
+ aim += ' ' + 'at home';
+
+console.log(aim);
+
+//please take note of the methods of adding spaces in your code.
+
+console.log( 'z' > 'A');
+console.log( 'Z' > 'a');
+console.log( 'Z' > 'A');
+console.log( 'A' > 'Z');
+console.log( 'a' > 'A');
+console.log( 'z' > 'Z');
+
+let band = 'Ere Asalatu';
+let song = 'Oni ni Jimoh';
+
+let olorin;
+    olorin = band + ": " + song;
+
+console.log(olorin)    
+
+let name;
+    name = "Pablo Escobar ";
+    name += 'The Boss of Bosses';
+
+    console.log(name);
+
+
+
