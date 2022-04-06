2fea3efcd2e8c717c6f639c740f29ef5679d4991
milk-maid <roqbell@gmail.com>
2022-04-06 11:58:45 +0100

    tring

--- domCart/DOMCart/js/app.js
+++ domCart/DOMCart/js/app.js
@@ -2,8 +2,14 @@ let element;
 
 element = document;
 
+// let heading = document.getElementById('heading');
+// heading.style.background = '#f93';
+// heading.style.color = '#a2b3c5';
+// heading.textContent = 'YOUR BEST CHOICE';
 
 
 
 
-console.log(element);
+
+
+console.log(heading);
